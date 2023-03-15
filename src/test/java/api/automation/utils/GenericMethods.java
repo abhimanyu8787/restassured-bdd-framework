f5d325e88f9f348d3f57427a904c08b031559d45
Abhimanyu <abhimanyukumar8787@gmail.com>
2023-03-15 23:43:31 +0530

    Added Method to find duplicate elements from string

--- src/test/java/api/automation/utils/GenericMethods.java
+++ src/test/java/api/automation/utils/GenericMethods.java
@@ -1,6 +1,10 @@
 package api.automation.utils;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.response.Response;
@@ -13,6 +17,18 @@ public class GenericMethods {
         String schemaFilePath = resourceAPI.getResource();
         File responseSchema = new File(schemaFilePath);
         response.then().assertThat().body(JsonSchemaValidator.matchesJsonSchema(responseSchema));
+    }
+    
+    public List<String> findDuplicates(List<String> stringList){
+        List<String> duplicatesList = new ArrayList<String>();
+        Set<String> uniqueList = new HashSet<String>();
+        for(String str : stringList) {
+            if(!uniqueList.contains(str))
+                uniqueList.add(str);
+            else
+                duplicatesList.add(str);
+        }
+        return duplicatesList;
     }
 
 }
