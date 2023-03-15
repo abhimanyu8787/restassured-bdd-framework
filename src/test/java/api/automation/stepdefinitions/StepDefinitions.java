c3c358431373973c98a5f82a5c948677f830b619
Abhimanyu <abhimanyukumar8787@gmail.com>
2023-03-15 23:42:55 +0530

    Added Support for Dependency Injection in Existing Stepdef

--- src/test/java/api/automation/stepdefinitions/StepDefinitions.java
+++ src/test/java/api/automation/stepdefinitions/StepDefinitions.java
@@ -1,13 +1,11 @@
 package api.automation.stepdefinitions;
 
 import static io.restassured.RestAssured.given;
-
 import java.io.IOException;
-
 import static org.junit.Assert.*;
-
 import api.automation.utils.APIResources;
 import api.automation.utils.GenericMethods;
+import api.automation.utils.TestContextSetup;
 import api.automation.utils.TestDataBuilder;
 import api.automation.utils.Utility;
 import io.cucumber.java.en.Given;
@@ -20,15 +18,17 @@ import io.restassured.specification.ResponseSpecification;
 
 public class StepDefinitions extends Utility{
     
-    RequestSpecification res;
-    ResponseSpecification resspec;
-    Response response;
-    TestDataBuilder testDataBuilder = new TestDataBuilder();
-    JsonPath responseJsonObject;
-    GenericMethods genericMethods;
+    public TestContextSetup testContextSetup;
+    public RequestSpecification res;
+    public ResponseSpecification resspec;
+    public Response response;
+    public TestDataBuilder testDataBuilder = new TestDataBuilder();
+    public JsonPath responseJsonObject;
+    public GenericMethods genericMethods;
     
-    public StepDefinitions() {
-        genericMethods = new GenericMethods();
+    public StepDefinitions(TestContextSetup testContextSetup) {
+        this.testContextSetup = testContextSetup;
+        this.genericMethods = testContextSetup.genericMethods;
     }
     
     @Given("List Users API with {string} and {string}")
@@ -45,18 +45,18 @@ public class StepDefinitions extends Utility{
             APIResources resourceAPI = APIResources.valueOf(resource);
             String pathURL = resourceAPI.getResource();
             System.out.println("Resource URL: "+pathURL);
-            response = res.when().get(pathURL);
+            testContextSetup.response = res.when().get(pathURL);
         }
     }
 
     @Then("verify if API returns with status code {int}")
     public void verify_if_api_returns_with_status_code(int statusCode) {
-        assertEquals(statusCode, response.getStatusCode());
+        assertEquals(statusCode, testContextSetup.response.getStatusCode());
     }
 
     @Then("verify API response against {string} schema")
     public void verify_api_response_against_schema(String schemaFileName) throws Exception {
-        genericMethods.validateWithSchema(response, schemaFileName);
+        genericMethods.validateWithSchema(testContextSetup.response, schemaFileName);
     }
     
     @Given("Register User API with {string} and {string}")
@@ -69,15 +69,15 @@ public class StepDefinitions extends Utility{
         if(resource.equals("RegisterUserAPI")) {
             APIResources resourceAPI = APIResources.valueOf(resource);
             String pathURL = resourceAPI.getResource();
-            response = res.when().post(pathURL);
+            testContextSetup.response = res.when().post(pathURL);
         }else if(resource.equals("LoginAPI")) {
             APIResources resourceAPI = APIResources.valueOf(resource);
             String pathURL = resourceAPI.getResource();
-            response = res.when().post(pathURL);
+            testContextSetup.response = res.when().post(pathURL);
         }else if(resource.equals("CreateUserAPI")) {
             APIResources resourceAPI = APIResources.valueOf(resource);
             String pathURL = resourceAPI.getResource();
-            response = res.when().post(pathURL);
+            testContextSetup.response = res.when().post(pathURL);
         }
     }
     
